35f973b5f5d6917b8cd0d375f798d8bc3881f4c8
Renee Staab <renee.staab09@gmail.com>
2018-10-11 22:03:49 -0400

    Added javascript, css & html styling

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const deck = document.querySelector('.deck');
+let toggledCards = [];
+let moves = 0;
+let clockOff = true;
+let time = 0;
+let clockId;
+let matched = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,6 +23,221 @@ function shuffle(array) {
 
     return array;
 }
+
+deck.addEventListener('click', event => {
+	const clickTarget = event.target;
+
+	if (isClickValid(clickTarget)) {
+		if (clockOff) {
+			startClock();
+			clockOff = false;
+		}
+	
+		toggleCard(clickTarget);
+		addToggleCard(clickTarget);
+
+	if (toggledCards.length === 2) {
+			checkForMatch(clickTarget);
+			addMove();
+			checkScore();
+		}
+	}
+})
+
+function toggleCard(card) {
+		card.classList.toggle('open');
+		card.classList.toggle('show');
+}
+
+function isClickValid(clickTarget) {
+	return (clickTarget.classList.contains('card') &&
+			!clickTarget.classList.contains('match') &&
+			toggledCards.length < 2 &&
+			!toggledCards.includes(clickTarget));
+}
+
+function addToggleCard(clickTarget) {
+	toggledCards.push(clickTarget);
+	console.log(toggledCards);
+}
+
+function checkForMatch() {
+   if (
+     toggledCards[0].firstElementChild.className ===
+     toggledCards[1].firstElementChild.className
+   ) {
+     toggledCards[0].classList.toggle("match");
+     toggledCards[1].classList.toggle("match");
+     toggledCards = [];
+     matched++;
+     checkWin();
+      }
+    else {
+     setTimeout(() => {
+     	toggleCard(toggledCards[0]);
+     	toggleCard(toggledCards[1]);
+     	toggledCards = [];
+   		}, 1000);
+   	}
+}
+
+function shuffleDeck() {
+	const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+	const shuffledCards = shuffle(cardsToShuffle);
+	for (card of shuffledCards) {
+		deck.appendChild(card);
+	}
+}
+
+function addMove() {
+	moves++;
+	const movesText = document.querySelector('.moves');
+	movesText.innerHTML = moves;
+}
+
+function checkScore() {
+	if (moves === 14 || moves === 20 || moves === 24 || moves === 30) {
+		hideStar();
+	}
+}
+
+function hideStar() {
+	const starList = document.querySelectorAll('.stars li');
+	for (star of starList) {
+		if (star.style.display != 'none') {
+			star.style.display = 'none';
+			break;
+		}
+	}
+}
+
+function startClock() {
+		clockId = setInterval(() => {
+		time++;
+		displayTime();
+	}, 1000);
+}
+
+function displayTime() {
+	const clock = document.querySelector('.clock');
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	if (seconds < 10) {
+		clock.innerHTML = `${minutes}:0${seconds}`;
+	} else {
+		clock.innerHTML = `${minutes}:${seconds}`;
+	}
+}
+
+function stopClock() {
+	clearInterval(clockId);
+}
+
+function getStars() {
+	stars = document.querySelectorAll('.stars li');
+	starCount = 0;
+	for (star of stars) {
+		if (star.style.display != 'none') {
+			starCount++;
+		}
+	}
+return starCount;
+}
+
+function toggleModal() {
+	const modal = document.querySelector('.modal__background');
+	modal.classList.toggle('hide');
+}
+
+function writeModalStats() {
+	const timeStat = document.querySelector('.modal__time');
+	const clockTime = document.querySelector('.clock').innerHTML;
+	const movesStat = document.querySelector('.modal__moves');
+	const starsStat = document.querySelector('.modal__stars');
+	const stars = getStars();
+
+	timeStat.innerHTML = `Time = ${clockTime}`;
+	movesStat.innerHTML = `Moves = ${moves}`;
+	starsStat.innerHTML = `Stars = ${stars}`;
+}
+
+
+document.querySelector('.modal__cancel').addEventListener('click', toggleModal);
+
+document.querySelector('.modal__replay').addEventListener('click', replayGame);
+
+document.querySelector('.modal__replay').addEventListener('click', resetGame);
+
+document.querySelector('.restart').addEventListener('click', resetGame);
+
+function resetGame() {
+	resetClockAndTime();
+	resetMoves();
+	resetStars();
+	resetCards();
+	shuffleDeck();
+	
+}
+
+function replayGame() {
+	matched = 0;
+	resetGame();
+	toggleModal();
+	resetCards();
+	resetStars();
+}
+
+function resetCards() {
+	const cards = document.querySelectorAll('.deck li');
+	for (let card of cards) {
+		card.className = 'card';
+	}
+}
+
+function resetClockAndTime() {
+	stopClock();
+	clockOff = true;
+	time = 0;
+	displayTime();
+}
+
+function resetMoves() {
+	moves = 0;
+	document.querySelector('.moves').innerHTML = moves;
+}
+
+function resetStars() {
+	stars = 0;
+	const starList = document.querySelectorAll('.stars li');
+	for (star of starList) {
+		star.style.display = 'inline';
+	}
+}
+
+function checkWin() {
+    matched += 1;
+    if (matched === 8) {
+        gameOver();
+    }
+};
+
+function gameOver() {
+	stopClock();
+	writeModalStats();
+	toggleModal();
+}
+
+shuffleDeck();
+writeModalStats();
+checkWin();
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
 
 
 /*
